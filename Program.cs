1fea8c7a3d54716d88af76e4e025566160c1bf8c
abinay-devulapally <abinaykumar.d99@gmail.com>
2024-06-01 23:16:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using StackExchange.Redis;
+
+namespace RedisTest
+{
+    internal class Program
+    {
+        private static string _connectionString = "";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            _connectionString = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+
+            ConnectionMultiplexer newConnection = ConnectionMultiplexer.Connect(_connectionString);
+
+            IDatabase db = newConnection.GetDatabase();
+
+            db.StringSet("mynewkey", "This is my test value");
+
+            string value = db.StringGet("mynewkey");
+
+            Console.WriteLine(value);
+        }
+    }
+}
